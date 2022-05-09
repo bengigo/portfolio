ceec10ca0a3248fa382f73d2e06b21b81ff3f786
bengigenc <genc.bengi@gmail.com>
2022-05-09 23:43:17 +0300

    Disable mobile menu background scroll

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector('#burger');
 const mobileNav = document.querySelector('.mobile-nav');
 const mobileLinks = document.querySelectorAll('.mobile-links');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   mobileNav.classList.toggle('active');
+  body.classList.toggle('fixed');
 });
 
 mobileLinks.forEach((link) => link.addEventListener('click', () => {
