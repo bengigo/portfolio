67485aa5d9e7b2888a02f8277bd5d45d91110f5a
bengigenc <genc.bengi@gmail.com>
2022-05-09 22:56:08 +0300

    toggle active classes in mobile links event listener

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ const mobileNav = document.querySelector('.mobile-nav');
 const mobileLinks = document.querySelectorAll('.mobile-links');
 
 burger.addEventListener('click', () => {
-  //toggle class active
   burger.classList.toggle('active');
   mobileNav.classList.toggle('active');
 });
 
-
-
 mobileLinks.forEach((link) => link.addEventListener('click', () => {
-  // alert('hello');
+  burger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
 }));
 
 console.log('end');
