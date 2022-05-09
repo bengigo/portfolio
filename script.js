886c867f1a50d13b9e8f6d05785c41921841a311
bengigenc <genc.bengi@gmail.com>
2022-05-09 22:51:56 +0300

    Fix linter Errors

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ const mobileNav = document.querySelector('.mobile-nav');
 const mobileLinks = document.querySelectorAll('.mobile-links');
 
 burger.addEventListener('click', () => {
-    //toggle class active
-    burger.classList.toggle('active');
-    mobileNav.classList.toggle('active');
+  //toggle class active
+  burger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
 });
 
 
 
-mobileLinks.forEach((link) =>
-    link.addEventListener('click', () => {
-        // alert('hello');
-    })
-);
+mobileLinks.forEach((link) => link.addEventListener('click', () => {
+  // alert('hello');
+}));
 
 console.log('end');
