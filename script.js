2a60c498aeec717fa8225e4d1b27b3656b2830df
Bengi Genç <genc.bengi@gmail.com>
2022-12-30 11:21:42 +0300

    Implement navigation using mobile menu

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const burger = document.querySelector('#burger');
 const menuContainer = document.querySelector('nav');
 const menu = document.querySelector('#menu');
+const navLinks = document.querySelectorAll('.nav-link')
+const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   menuContainer.classList.toggle('active');
   menu.classList.toggle('active');
+  body.classList.toggle('fixed');
 })
+
+navLinks.forEach((link) => link.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menuContainer.classList.toggle('active');
+  menu.classList.toggle('active');
+
+}))
