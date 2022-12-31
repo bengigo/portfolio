21cadbad18070632767dde376432ca00cb657dcc
Bengi Genç <genc.bengi@gmail.com>
2022-12-31 20:34:14 +0300

    Store background colors selection in an array

--- script.js
+++ script.js
@@ -1,82 +1,85 @@
-const burger = document.querySelector('#burger');
-const menuContainer = document.querySelector('nav');
-const menu = document.querySelector('#menu');
-const navLinks = document.querySelectorAll('.nav-link');
-const body = document.querySelector('body');
-const portfolioContainer = document.querySelector('#works');
+const burger = document.querySelector("#burger");
+const menuContainer = document.querySelector("nav");
+const menu = document.querySelector("#menu");
+const navLinks = document.querySelectorAll(".nav-link");
+const body = document.querySelector("body");
+const portfolioContainer = document.querySelector("#works");
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  menuContainer.classList.toggle('active');
-  menu.classList.toggle('active');
-  body.classList.toggle('fixed');
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  menuContainer.classList.toggle("active");
+  menu.classList.toggle("active");
+  body.classList.toggle("fixed");
 });
 
-navLinks.forEach((link) => link.addEventListener('click', () => {
-  burger.classList.remove('active');
-  menuContainer.classList.remove('active');
-  body.classList.remove('fixed');
-}));
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    menuContainer.classList.remove("active");
+    body.classList.remove("fixed");
+  })
+);
 
 const worksData = [
   {
-    title: 'Leaderboard',
-    mobileImg: 'assets/works/desktop/leaderboard.png',
-    desktopImg: 'assets/works/desktop/leaderboard.png',
+    title: "Leaderboard",
+    mobileImg: "assets/works/desktop/leaderboard.png",
+    desktopImg: "assets/works/desktop/leaderboard.png",
     description:
-      'A web app for you and your friends to add your scores of your favorite game -Terraria. Happy gaming!',
-    badge1: 'HTML',
-    badge2: 'CSS',
-    badge3: 'JavaScript',
-    badge4: 'Webpack',
-    live: 'https://bengigo.github.io/leaderboard/',
-    source: 'https://github.com/bengigo/leaderboard',
+      "A web app for you and your friends to add your scores of your favorite game -Terraria. Happy gaming!",
+    badge1: "HTML",
+    badge2: "CSS",
+    badge3: "JavaScript",
+    badge4: "Webpack",
+    live: "https://bengigo.github.io/leaderboard/",
+    source: "https://github.com/bengigo/leaderboard",
   },
   {
-    title: 'Gastronomy Camp 2022',
-    mobileImg: 'assets/works/mobile/gastronomy-events.png',
-    desktopImg: 'assets/works/desktop/gastronomy-events.png',
+    title: "Gastronomy Camp 2022",
+    mobileImg: "assets/works/mobile/gastronomy-events.png",
+    desktopImg: "assets/works/desktop/gastronomy-events.png",
     description:
-      'A website to give information about a gastronomy-related camping event. You can find information about the program and speakers. Built with HTML-CSS-JavaScript',
-    badge1: 'HTML',
-    badge2: 'CSS',
-    badge3: 'JavaScript',
-    badge4: '',
-    live: 'https://bengigo.github.io/gastronomy-camp-2022/',
-    source: 'https://github.com/bengigo/gastronomy-camp-2022',
-  }, 
-  {
-    title: 'To-Do List',
-    mobileImg: 'assets/works/mobile/todo-list.png',
-    desktopImg: 'assets/works/desktop/todo-list.png',
-    description:
-      'A ToDo List with a simple design. Ideal for keeping track of simple daily tasks.',
-    badge1: 'HTML',
-    badge2: 'CSS',
-    badge3: 'JavaScript',
-    badge4: 'Webpack',
-    live: 'https://bengigo.github.io/to-do-list/',
-    source: 'https://github.com/bengigo/to-do-list',
+      "A website to give information about a gastronomy-related camping event. You can find information about the program and speakers. Built with HTML-CSS-JavaScript",
+    badge1: "HTML",
+    badge2: "CSS",
+    badge3: "JavaScript",
+    badge4: "",
+    live: "https://bengigo.github.io/gastronomy-camp-2022/",
+    source: "https://github.com/bengigo/gastronomy-camp-2022",
   },
   {
-    title: 'Awesome Books',
-    mobileImg: 'assets/works/mobile/awesome-books.png',
-    desktopImg: 'assets/works/desktop/awesome-books.png',
+    title: "To-Do List",
+    mobileImg: "assets/works/mobile/todo-list.png",
+    desktopImg: "assets/works/desktop/todo-list.png",
     description:
-      'A single page application to create a list of a book collection. Can be used to create a collection of read books. Or as a wishlist of books.',
-    badge1: 'HTML',
-    badge2: 'CSS',
-    badge3: 'JavaScript',
-    badge4: '',
-    live: 'https://bengigo.github.io/awesome-books-modules/',
-    source: 'https://github.com/bengigo/awesome-books-modules',
+      "A ToDo List with a simple design. Ideal for keeping track of simple daily tasks.",
+    badge1: "HTML",
+    badge2: "CSS",
+    badge3: "JavaScript",
+    badge4: "Webpack",
+    live: "https://bengigo.github.io/to-do-list/",
+    source: "https://github.com/bengigo/to-do-list",
+  },
+  {
+    title: "Awesome Books",
+    mobileImg: "assets/works/mobile/awesome-books.png",
+    desktopImg: "assets/works/desktop/awesome-books.png",
+    description:
+      "A single page application to create a list of a book collection. Can be used to create a collection of read books. Or as a wishlist of books.",
+    badge1: "HTML",
+    badge2: "CSS",
+    badge3: "JavaScript",
+    badge4: "",
+    live: "https://bengigo.github.io/awesome-books-modules/",
+    source: "https://github.com/bengigo/awesome-books-modules",
   },
 ];
 
 worksData.forEach((work) => {
-  const cardContainer = document.createElement('div');
+  const cardContainer = document.createElement("div");
   portfolioContainer.appendChild(cardContainer);
-  cardContainer.classList.add("card-container")
+  cardContainer.classList.add("card-container");
+
   cardContainer.innerHTML = `
   <div class="mobile img-container">
     <img class="work-img" src="${work.mobileImg}" alt="">
@@ -94,3 +97,17 @@ worksData.forEach((work) => {
   </ul>
   `;
 });
+
+const bgColors = [
+  "#66bfbf",
+  "#a69ab1",
+  "#9fd3c7",
+  "#ea9085",
+  "#c4c1e0",
+  "#f8b595",
+  "#c3bef0",
+  "#29a19cb5",
+  "#bbded6",
+  "#719192",
+];
+
